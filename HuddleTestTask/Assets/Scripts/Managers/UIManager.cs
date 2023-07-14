e8aa429d0d143e1dd7b4782e55a65207440a3e06
Umair <umairwasim001@gmail.com>
2023-07-14 20:41:22 +0500

    Add Menu states

--- HuddleTestTask/Assets/Scripts/Managers/UIManager.cs
+++ HuddleTestTask/Assets/Scripts/Managers/UIManager.cs
@@ -1,22 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+public enum MenuState
+{
+    Main,
+    Game,
+    Won,
+    Lost,
+}
+
 public class UIManager : MonoBehaviour
 {
+    public MenuState menu = MenuState.Main;
+
     [Header("Menus")]
-    [SerializeField] private GameObject mainMenu;
-    [SerializeField] private GameObject gameplayMenu;
-    [SerializeField] private GameObject gameWonMenu;
-    [SerializeField] private GameObject gameLostMenu;
+    [SerializeField] private GameObject[] menus; // 0 = main, 1 = game, 2 = won, 3 = lost
 
     [Header("Buttons")]
     [SerializeField] private Button playButton;
+    [SerializeField] private Button wonButton;
+    [SerializeField] private Button lostButton;
+
+    private void Start()
+    {
+        for (int i = 0; i < menus.Length; i++)
+        {
+            menus[i].SetActive(false);
+        }
+        //Enable the main menu only
+        menus[(int)MenuState.Main].SetActive(true);
+    }
 
     public void PlayButtonPressed()
     {
+        menus[(int)MenuState.Main].SetActive(false);
+        GameManager.Instance.ChangeGameState(GameState.GenerateGrid);
     }
 
     public void RestartGame()
